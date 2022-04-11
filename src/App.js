bde0febb7a4b7baadb2d7dd0802c2ad4de5c724c
Radoslav Grencik <grencik@inqool.cz>
2022-04-11 22:56:49 +0200

    added menu app bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "@mui/material/Button";
+import MenuAppBar from "./components/MenuAppBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button
-          variant="contained"
-          onClick={() => {
-            alert("ALERT");
-          }}
-        >
-          Learn React
-        </Button>
-      </header>
+    <div>
+      <MenuAppBar />
     </div>
   );
 }
