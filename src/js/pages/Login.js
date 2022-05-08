76ecb1ea9db5575f6c62063cb68273659a340526
Radoslav Grencik <grencik@inqool.cz>
2022-05-08 18:33:40 +0200

    minor fix

--- src/js/pages/Login.js
+++ src/js/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { apiFetch } from "../utils/apiFetch";
@@ -12,70 +12,51 @@ const Login = () => {
   const [formErrorMsg, setFormErrorMsg] = useState("");
   const navigate = useNavigate();
 
+  const submitForm = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const response = await apiFetch({
+        route: "/login",
+        method: "POST",
+        body: loginData.current,
+      });
+      if (response.status === 200) {
+        setFormError(false);
+        setFormErrorMsg("");
+        setUserToken(response.data?.token);
+        navigate("/splash", { replace: true });
+      } else {
+        setFormError(true);
+        setFormErrorMsg(response.data?.message);
+      }
+    },
+    [navigate, setUserToken]
+  );
+
   return (
     <main className="login container--default flex--grow flex">
       <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          const response = await apiFetch({
-            route: "/login",
-            method: "POST",
-            body: loginData.current,
-          });
-          if (response.status === 200) {
-            setFormError(false);
-            setFormErrorMsg("");
-            setUserToken(response.data?.token);
-            navigate("/splash", { replace: true });
-          } else {
-            setFormError(true);
-            setFormErrorMsg(response.data?.message);
-          }
-        }}
+        onSubmit={submitForm}
         className="login__form flex--grow flex flex--column flex--justify-center flex--align-center"
       >
         <h2>Přihlášení</h2>
-        {formError ? (
-          <>
-            <TextField
-              onChange={(event) =>
-                (loginData.current.login = event.target.value)
-              }
-              label="Login"
-              variant="outlined"
-              error
-              helperText={formErrorMsg}
-            />
-            <TextField
-              onChange={(event) =>
-                (loginData.current.password = event.target.value)
-              }
-              label="Heslo"
-              variant="outlined"
-              type={"password"}
-              error
-              helperText={formErrorMsg}
-            />
-          </>
-        ) : (
-          <>
-            <TextField
-              onChange={(event) =>
-                (loginData.current.login = event.target.value)
-              }
-              label="Login"
-              variant="outlined"
-            />
-            <TextField
-              onChange={(event) =>
-                (loginData.current.password = event.target.value)
-              }
-              label="Heslo"
-              variant="outlined"
-              type={"password"}
-            />
-          </>
-        )}
+        <TextField
+          onChange={(event) => (loginData.current.login = event.target.value)}
+          label="Login"
+          variant="outlined"
+          error={formError}
+          helperText={formErrorMsg}
+        />
+        <TextField
+          onChange={(event) =>
+            (loginData.current.password = event.target.value)
+          }
+          label="Heslo"
+          variant="outlined"
+          type={"password"}
+          error={formError}
+          helperText={formErrorMsg}
+        />
         <Button
           type="submit"
           sx={{ width: 210, height: 56 }}
